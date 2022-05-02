9df47db20ad9bd70842a1a3a5a45620587d6c5d4
kartikey sood <kartikeysood@rediffmail.com>
2022-05-02 13:03:45 +0530

    foreign key

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,34 +1,10 @@
 const Post = require('./Post')
 const users = require('./users')
 module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define('Comment', {
+  const Comment = sequelize.define('comment', {
     body: {
       type: DataTypes.TEXT,
       allowNull: false,
-    },
-    author_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'users',
-        key: 'id',
-      },
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'posts',
-        key: 'id',
-      },
-    },
-    parent_comment_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'comments',
-        key: 'id',
-      },
     },
   })
 
